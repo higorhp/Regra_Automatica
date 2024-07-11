f9a00093181026ad8f2cfe7f130c9de84d996e7e
Higor <112891205+higorhp@users.noreply.github.com>
2024-07-11 17:39:05 -0300

    2.0

--- interface.py
+++ interface.py
@@ -12,6 +12,8 @@ import sys
 from automacao_acesso import executar_automacao, salvar_no_final, clicar_pagina_x_usuarios
 from reset_senhas import selecionar_arquivo, processar_logins
 from matricula_senha import selecionar_arquivo_matri, processar_logins_matri
+from clicar_recursos import clicar_vr,clicar_vt,clicar_ao, clicar_admissa,clicar_fer,clicar_resci,clicar_CRM,clicar_va,clicar_SRA
+
 
 
 diretorio_atual = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
@@ -57,6 +59,33 @@ def iniciar_automacao():
     if pagina_var.get() == 1:
         ultimo_nome = nomes[-1].strip() if nomes else ""
         clicar_pagina_x_usuarios(ultimo_nome, sistemas)
+
+    if va_var.get() == 1:
+        clicar_va()
+
+    if vr_var.get() == 1:
+        clicar_vr()
+    
+    if vt_var.get() == 1:
+        clicar_vt()
+
+    if ao_var.get() == 1:
+        clicar_ao()
+
+    if  admissao_var.get() == 1:
+        clicar_admissa()
+    
+    if rescisao_var.get() == 1:
+        clicar_resci()
+
+    if ferias_var.get() == 1:
+        clicar_fer()
+    
+    if crm_var.get() == 1:
+        clicar_CRM()
+
+    if sra_var.get() ==1:
+        clicar_SRA()
     
 
 def importar_usuarios():
@@ -125,28 +154,41 @@ def abrir_janela_log():
 root = tk.Tk()
 root.title("Regra de acesso automática")
 root.configure(bg="#add8e6")
-root.geometry("420x460")
+root.geometry("590x500")
 root.resizable(False, False)  # Isso impede que a janela seja redimensionada
 
 entry_width = 32
 
-# Estilo para o Notebook
-style = ttk.Style()
-style.configure('TNotebook.Tab', background="#6495ED", padding=[50, 1])
-style.map('TNotebook.Tab', background=[('selected', '#add8e6')])
-style.configure('TNotebook', tabposition='n') 
+# Frame para organizar os dois Notebooks
+main_frame = tk.Frame(root)
+main_frame.pack(fill='both', expand=True)
 
-# Cria o Notebook (abas)
-notebook = ttk.Notebook(root, style='TNotebook')
-notebook.pack(padx=0, pady=0, fill='both', expand=True)
+# Notebook à esquerda para "Página x Usuário"
+style_left = ttk.Style()
+style_left.configure('Left.TNotebook.Tab', background="#6495ED", padding=[5, 1])
+style_left.map('Left.TNotebook.Tab', background=[('selected', '#add8e6')])
 
-# Cria o frame para a aba de Regra de Acesso
-frame_regra_acesso = tk.Frame(notebook, bg="#add8e6")
-notebook.add(frame_regra_acesso, text="Regra de Acesso")
+notebook_left = ttk.Notebook(main_frame, style='Left.TNotebook')
+notebook_left.pack(side='left', fill='both', expand=True)
 
-# Cria o frame para a aba de Reset de Senhas
-frame_reset_senhas = tk.Frame(notebook, bg="#add8e6")
-notebook.add(frame_reset_senhas, text="Controle de Acesso")
+# Notebook para as outras abas
+style_right = ttk.Style()
+style_right.configure('Right.TNotebook.Tab', background="#6495ED", padding=[50, 1])
+style_right.map('Right.TNotebook.Tab', background=[('selected', '#add8e6')])
+
+notebook_right = ttk.Notebook(main_frame, style='Right.TNotebook')
+notebook_right.pack(side='left', fill='both', expand=True)
+
+# Cria o frame para a aba de Página x Usuário
+frame_pagina_usuario = tk.Frame(notebook_left, bg="#add8e6")
+notebook_left.add(frame_pagina_usuario, text="Página x Usuário")
+
+# Cria os frames para as outras abas
+frame_regra_acesso = tk.Frame(notebook_right, bg="#add8e6")
+notebook_right.add(frame_regra_acesso, text="Regra de Acesso")
+
+frame_reset_senhas = tk.Frame(notebook_right, bg="#add8e6")
+notebook_right.add(frame_reset_senhas, text="Controle de Acesso")
 
 # Centraliza os widgets da aba de Regra de Acesso
 titulo_janela = tk.Label(frame_regra_acesso, text="Regra de Acesso Automática", bg="#add8e6", fg="black", font=("Roboto", 16, "bold"))
@@ -170,7 +212,7 @@ sistemas_listbox = tk.Listbox(frame_regra_acesso, selectmode=tk.MULTIPLE, width=
 sistemas_listbox.grid(row=4, column=1, padx=10, pady=5, sticky="w")
 
 # Adicionar sistemas à listbox
-sistemas = ["MOVIMENTAÇÃO RECURSOS HUMANOS", "CONTROLE POLICIA FEDERAL", "TELEFONIA", "OCORRENCIA","PONTO WEB","REVISÃO", "CONTRATOS", "SISTEMA DE DOCUMENTOS", "SISTEMA DE BENEFÍCIOS", "EXPORTACAO", "GPS 360"]
+sistemas = ["MOVIMENTAÇÃO RECURSOS HUMANOS", "CONTROLE POLICIA FEDERAL", "TELEFONIA", "OCORRENCIA","PONTO WEB","REVISÃO", "CONTRATOS", "SISTEMA DE DOCUMENTOS", "SISTEMA DE BENEFÍCIOS", "EXPORTACAO","ALTERAÇÃO CR", "GPS 360"]
 for sistema in sistemas:
     sistemas_listbox.insert(tk.END, sistema)
 
@@ -187,7 +229,7 @@ salvar_var = tk.IntVar()
 salvar_checkbutton = tk.Checkbutton(frame_regra_acesso, text="Salvar Regras", variable=salvar_var, bg="#add8e6", fg="black", font=("Roboto", 10, "bold"))
 salvar_checkbutton.grid(row=7, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
-# Checkbutton para executar clicar_pagina_x_usuarios
+# Checkbutton para executar clicar_pagina_x_usuario
 pagina_var = tk.IntVar()
 pagina_checkbutton = tk.Checkbutton(frame_regra_acesso, text="Pagina x Usuários", variable=pagina_var, bg="#add8e6", fg="black", font=("Roboto", 10, "bold"))
 pagina_checkbutton.grid(row=8, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
@@ -198,6 +240,69 @@ formato_button.grid(row=9, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
 
 # Adicionando botão de Log
 tk.Button(frame_regra_acesso, text="Log", command=abrir_janela_log, bg="#FF6347", fg="white", font=("Roboto", 10, "bold")).grid(row=8, column=1, padx=10, pady=5, sticky="e")
+
+##################### Página x Usuário opções ##############################
+va_var = tk.IntVar()
+vr_var = tk.IntVar()
+vt_var = tk.IntVar()
+ao_var = tk.IntVar()
+ferias_var = tk.IntVar()
+rescisao_var = tk.IntVar()
+admissao_var = tk.IntVar()
+crm_var = tk.IntVar()
+sra_var = tk.IntVar()
+
+label_pergunta = tk.Label(frame_pagina_usuario, text="Pastas:", bg="#add8e6", fg="black", font=("Roboto", 12, "bold"))
+label_pergunta.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky="n")
+
+#Benefícios
+
+label_beneficios = tk.Label(frame_pagina_usuario, text="Benefícios:", bg="#add8e6", fg="black", font=("Roboto", 12, "bold"))
+label_beneficios.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="n")
+
+checkbox_va = tk.Checkbutton(frame_pagina_usuario, text="VA", variable=va_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_va.grid(row=2, column=1, padx=10, pady=5, sticky="w")
+
+checkbox_vr = tk.Checkbutton(frame_pagina_usuario, text="VR", variable=vr_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_vr.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+
+checkbox_vt = tk.Checkbutton(frame_pagina_usuario, text="VT", variable=vt_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_vt.grid(row=3, column=0, padx=10, pady=5, sticky="w")
+
+checkbox_ao = tk.Checkbutton(frame_pagina_usuario, text="AO", variable=ao_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_ao.grid(row=3, column=1, padx=10, pady=5, sticky="w")
+
+#MRH
+
+label_mrh = tk.Label(frame_pagina_usuario, text="MRH:", bg="#add8e6", fg="black", font=("Roboto", 12, "bold"))
+label_mrh.grid(row=5, column=0, columnspan=2, padx=10, pady=10, sticky="n")
+
+checkbox_admissa = tk.Checkbutton(frame_pagina_usuario, text="Admissão", variable=admissao_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_admissa.grid(row=6, column=0, padx=10, pady=5, sticky="w")
+
+checkbox_res = tk.Checkbutton(frame_pagina_usuario, text="Rescisão", variable=rescisao_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_res.grid(row=7, column=0, padx=10, pady=5, sticky="w")
+
+checkbox_fer = tk.Checkbutton(frame_pagina_usuario, text="Férias", variable=ferias_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_fer.grid(row=8, column=0, padx=10, pady=5, sticky="w")
+
+#CRM
+label_crm = tk.Label(frame_pagina_usuario, text="Ocorrência:", bg="#add8e6", fg="black", font=("Roboto", 12, "bold"))
+label_crm.grid(row=9, column=0, columnspan=2, padx=10, pady=10, sticky="n")
+
+checkbox_crm = tk.Checkbutton(frame_pagina_usuario, text="CRM", variable=crm_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_crm.grid(row=10, column=0, padx=10, pady=5, sticky="w")
+
+
+#SRA
+label_sra = tk.Label(frame_pagina_usuario, text="Exportação:", bg="#add8e6", fg="black", font=("Roboto", 12, "bold"))
+label_sra.grid(row=11, column=0, columnspan=2, padx=10, pady=10, sticky="n")
+
+checkbox_sra = tk.Checkbutton(frame_pagina_usuario, text="SRA", variable=sra_var, bg="#add8e6", fg="black", font=("Roboto", 9, "bold"))
+checkbox_sra.grid(row=12, column=0, padx=10, pady=5, sticky="w")
+
+
+######################################################################################################
 
 # Variável para armazenar a escolha do usuário
 escolha_var = tk.StringVar(value="")
